a99e390bc018b8e102cc7bfcbe3a1f39e84d5f57
Amar Kumar <amar2mail9@gmail.com>
2025-06-17 18:27:46 +0530

    prview spinner add

--- src/page/Posts/AllPost.jsx/PreviewBlog.jsx
+++ src/page/Posts/AllPost.jsx/PreviewBlog.jsx
@@ -2,6 +2,7 @@ import { Box, CardMedia, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { data } from "react-router-dom";
+import Spinner from "../../../components/Spinner";
 
 const PreviewBlog = ({ slug }) => {
   const [blog, setBlog] = useState(null);
@@ -25,30 +26,36 @@ const PreviewBlog = ({ slug }) => {
   return (
     <div>
       <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 md:h-[95vh] h-[75vh] -translate-y-1/2 min-w-sm max-w-full  bg-gray-900 border-1 border-gray-600 rounded-lg shadow-2xl">
-        <Box className="h-[35%] rounded-2xl">
-          <CardMedia
-            component="img"
-            className="h-full object-center object-cover rounded-t-lg"
-            image={`${blog?.thumbnail}`}
-            alt={`${blog?.title}`}
-          />
-        </Box>
-        <Typography
-          id="modal-modal-title"
-          variant="h6"
-          className="px-4"
-          component="h2"
-        >
-          {blog?.title || "Untitled"}
-        </Typography>
+        {blog === null ? (
+          <Spinner />
+        ) : (
+          <Box>
+            <Box className="h-[35%] rounded-2xl">
+              <CardMedia
+                component="img"
+                className="h-full object-center object-cover rounded-t-lg"
+                image={`${blog?.thumbnail}`}
+                alt={`${blog?.title}`}
+              />
+            </Box>
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              className="px-4"
+              component="h2"
+            >
+              {blog?.title || "Untitled"}
+            </Typography>
 
-        <Box
-          className="h-[58%] scrollable-container px-4 overflow-y-scroll"
-          id="modal-modal-description"
-          component="div"
-          sx={{ mt: 2 }}
-          dangerouslySetInnerHTML={{ __html: blog?.content }}
-        />
+            <Box
+              className="h-[58%] scrollable-container px-4 overflow-y-scroll"
+              id="modal-modal-description"
+              component="div"
+              sx={{ mt: 2 }}
+              dangerouslySetInnerHTML={{ __html: blog?.content }}
+            />
+          </Box>
+        )}
       </Box>
     </div>
   );
