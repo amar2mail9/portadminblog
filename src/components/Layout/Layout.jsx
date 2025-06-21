00b134d8d1132f42259b35aae09a7dc7d201ae13
Amar Kumar <amar2mail9@gmail.com>
2025-06-21 09:16:19 +0530

    Color cahnge and text editor small device

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -67,10 +67,10 @@ const Layout = ({ children }) => {
                     to={item.path}
                     key={index}
                     onClick={handleClose}
-                    className={`flex mb-2 items-center gap-2 py-2 px-4 text-white rounded-lg hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 
+                    className={`flex mb-2 items-center gap-2 py-2 px-4 text-gray-800 rounded-lg hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500 
                       ${
                         isActive
-                          ? "bg-gradient-to-r from-pink-600 to-pink-800 text-white shadow-lg"
+                          ? "bg-gradient-to-r from-indigo-600 to-purple-800 text-white shadow-lg"
                           : "bg-transparent"
                       }`}
                   >
